1c4a4724c11faf6614a8cc403f219439c4fec032
Justine-Frigo <justine0912@gmail.com>
2024-05-13 14:26:37 +0200

    add loader + style change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 // async function pour récupérer une citation
 async function fetchQuote() {
   try {
+    // Afficher le loader
+    document.getElementById("loader").style.display = "block";
     const response = await fetch("https://thatsthespir.it/api");
     if (!response.ok) {
       throw new Error("Failed to fetch quote");
     }
     const data = await response.json();
+    // Masquer le loader une fois que la citation est chargée
+    document.getElementById("loader").style.display = "none";
     return data;
   } catch (error) {
     console.error(error);
@@ -18,12 +22,11 @@ function renderQuote(quote) {
   const quoteContainer = document.getElementById("quoteContainer");
   quoteContainer.innerHTML = `
         <div class="quote">
-        <img src="${quote.photo == '' || quote.photo.includes('capture') ? './assets/anonymous_emblem.png' : quote.photo}" alt="${quote.author}">
+        <img src="${quote.photo == '' || quote.photo.includes('capture') ? './assets/images/anonymous_emblem.png' : quote.photo}" alt="${quote.author}">
         <div class="quoteBlock">
             <blockquote>${quote.quote}</blockquote>
             <p>- ${quote.author}</p></div>
         </div>
-        <button id="randomBtn">Generate random quote</button>
     `;
   document.getElementById("randomBtn").addEventListener("click", generateQuote);
 }
